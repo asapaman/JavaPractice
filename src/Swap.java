cdace6429b236a02e137ede7a120ac8b2aecd6ca
Aman Singh <asapaman@gmail.com>
2024-10-01 14:23:39 +0530

    Create Swap.java
    change

---
+++ src/Swap.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Swap {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Tw Numners");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        a =  a-b;
+        b= a+b;
+        a = b-a;
+        System.out.println(a+" "+b);
+
+
+    }
+}
