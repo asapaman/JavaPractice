6060aec4537a98e8be8056df738a96966905e5c1
Aman Singh <asapaman@gmail.com>
2025-06-23 23:17:49 +0530

    ok

--- src/Swap.java
+++ src/Swap.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Swap {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Two Numners :");
+        System.out.println("Enter Two Numbers :");
         int a = sc.nextInt();
         int b = sc.nextInt();
         a =  a-b;
