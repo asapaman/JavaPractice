8a7e485c3f335430d332ab06f14d18369ccb04d3
Aman Singh <asapaman@gmail.com>
2025-07-18 23:35:26 +0530

    ok

---
+++ src/TypeConversion.java
@@ -0,0 +1,12 @@
+public class TypeConversion {
+    public static void main(String[] args) {
+        int num = 10;
+        double d = num;
+
+        float dl = 555.5f;
+        int num2 = (int)dl;
+
+        System.out.println("Type Casting = "+ num2);
+        System.out.println("Type Conversion = "+ d);
+    }
+}
